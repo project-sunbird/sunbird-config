25a95bce6a04c9fc2181da5654b7d017cf88c366
G S Bajaj <gourishanker.bajaj@tarento.com>
2018-08-30 15:16:41 +0530

    issue #6496 feat: refresh and read APIs

--- config-base/manager/src/main/java/org/sunbird/config/util/CloudStore.java
+++ config-base/manager/src/main/java/org/sunbird/config/util/CloudStore.java
@@ -34,24 +34,7 @@ public class CloudStore {
         }else {
             throw new ServerException("ERR_INVALID_CLOUD_STORAGE", "Error while initialising cloud storage");
         }
-
-
-//        try{
-//            TelemetryManager.error("in cloud store");
-//            String storageKey = "bajaj";
-//            String storageSecret = "n9BdvADbFsoKHlGDtBbwROtJ5bDraxRo0lp/39iZqPbERuPpzzWuMlV+k8qMWaGO9D9LcB4aSjJWmtkO6mWgDA==";
-//            storageService = StorageServiceFactory.getStorageService(new StorageConfig(cloudStoreType, storageKey, storageSecret));
-//            TelemetryManager.error("in cloud store after initialization");
-//        } catch(Exception e){
-//            TelemetryManager.error("Get file size from Azure | Exception: " + e.getMessage(), e);
-//        }
-
-
     }
-
-//    public static BaseStorageService getCloudStoreService() {
-//        return storageService;
-//    }
 
     public static String getContainerName() {
         if(StringUtils.equalsIgnoreCase(cloudStoreType, "azure")) {
@@ -63,36 +46,20 @@ public class CloudStore {
         }
     }
 
-//    public static String[] uploadFile(String folderName, File file, boolean slugFile) throws Exception {
-//        if (BooleanUtils.isTrue(slugFile))
-//            file = Slug.createSlugFile(file);
-//        String objectKey = folderName + "/" + file.getName();
-//        String container = getContainerName();
-//        String url = storageService.upload(container, file.getAbsolutePath(), objectKey, Option.apply(false), Option.apply(false), Option.empty(), Option.empty());
-//        return new String[] { objectKey, url};
-//    }
-
-
     public static Blob getObject(String key) throws Exception {
-        System.out.println("In get object size");
         String container = getContainerName();
-        System.out.println("Container: " + container);
         Blob blob = null;
         blob = (Blob) storageService.getObject(container, key, Option.apply(false));
         return blob;
     }
 
     public static String getObjectData(String key) throws Exception {
-        System.out.println("In get object size");
         String container = getContainerName();
-        System.out.println("Container: " + container);
         String data[];
         data = storageService.getObjectData(container, key);
 
         String str = StringUtils.join(data);
-        System.out.println("Joined Data: " + str);
         String response = StringEscapeUtils.unescapeJava(str);
-        System.out.println("Unescaped Data: " + response);
         return response;
     }
 
@@ -108,23 +75,4 @@ public class CloudStore {
         String blobString = blob.toString();
         return blobString;
     }
-
-//    public static void copyObjectsByPrefix(String sourcePrefix, String destinationPrefix) {
-//        String container = getContainerName();
-//        storageService.copyObjects(container, sourcePrefix, container, destinationPrefix, Option.apply(true));
-//    }
-//
-//    public static String getURL(String prefix) {
-//        String container = getContainerName();
-//        Blob blob =  (Blob)storageService.getObject(container, prefix, Option.apply(false));
-//        Map<String, Object> map = scala.collection.JavaConversions.mapAsJavaMap(blob.metadata());
-//        return (String)map.get("uri");
-//    }
-//
-//
-//    public static void deleteFile(String key, boolean isDirectory) throws Exception {
-//        String container = getContainerName();
-//        storageService.deleteObject(container, key, Option.apply(isDirectory));
-//    }
-
 }
