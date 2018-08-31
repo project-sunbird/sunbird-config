1f27dcc69738916a9e00552b7d1381b39aee1209
G S Bajaj <gourishanker.bajaj@tarento.com>
2018-08-31 19:55:54 +0530

    issue #6496 feat: add load config on service start

--- config-base/manager/src/main/java/org/sunbird/config/util/CloudStore.java
+++ config-base/manager/src/main/java/org/sunbird/config/util/CloudStore.java
@@ -2,18 +2,21 @@ package org.sunbird.config.util;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
-//import Platform;
+import org.sunbird.cloud.storage.BaseStorageService;
+import org.sunbird.cloud.storage.Model.Blob;
+import org.sunbird.cloud.storage.factory.StorageConfig;
+import org.sunbird.cloud.storage.factory.StorageServiceFactory;
 import org.sunbird.common.Platform;
 import org.sunbird.common.exception.ServerException;
 import org.sunbird.common.util.S3PropertyReader;
 import org.sunbird.telemetry.logger.TelemetryManager;
-import org.sunbird.cloud.storage.BaseStorageService;
-import org.sunbird.cloud.storage.Model.Blob;
-//import org.sunbird.cloud.storage.conf.AppConf;
-import org.sunbird.cloud.storage.factory.StorageConfig;
-import org.sunbird.cloud.storage.factory.StorageServiceFactory;
-
 import scala.Option;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//import Platform;
+//import org.sunbird.cloud.storage.conf.AppConf;
 //import scala.collection.JavaConversions;
 
 public class CloudStore {
@@ -22,30 +25,27 @@ public class CloudStore {
     private static String cloudStoreType = Platform.config.getString("cloud_storage_type");
 
     static {
-
-        System.out.println(Platform.config.entrySet());
-
-        if(StringUtils.equalsIgnoreCase(cloudStoreType, "azure")) {
+        if (StringUtils.equalsIgnoreCase(cloudStoreType, "azure")) {
             String storageKey = Platform.config.getString("azure_storage_key");
             String storageSecret = Platform.config.getString("azure_storage_secret");
             storageService = StorageServiceFactory.getStorageService(new StorageConfig(cloudStoreType, storageKey, storageSecret));
-        }else if(StringUtils.equalsIgnoreCase(cloudStoreType, "aws")) {
+        } else if (StringUtils.equalsIgnoreCase(cloudStoreType, "aws")) {
             String storageKey = Platform.config.getString("aws_storage_key");
             String storageSecret = Platform.config.getString("aws_storage_secret");
             storageService = StorageServiceFactory.getStorageService(new StorageConfig(cloudStoreType, storageKey, storageSecret));
-        }else {
+        } else {
             TelemetryManager.error("Cloud storage type 2: " + Platform.config.getString("cloud_storage_type"));
             TelemetryManager.error("Azure storage key 2: " + Platform.config.getString("azure_storage_key"));
-            throw new ServerException("ERR_INVALID_CLOUD_STORAGE", "Error while initialising cloud storage, cst: " + Platform.config.getString("cloud_storage_type") + "***");
+            throw new ServerException("ERR_INVALID_CLOUD_STORAGE", "Error while initialising cloud storage");
         }
     }
 
     public static String getContainerName() {
-        if(StringUtils.equalsIgnoreCase(cloudStoreType, "azure")) {
+        if (StringUtils.equalsIgnoreCase(cloudStoreType, "azure")) {
             return Platform.config.getString("azure_storage_container");
-        }else if(StringUtils.equalsIgnoreCase(cloudStoreType, "aws")) {
+        } else if (StringUtils.equalsIgnoreCase(cloudStoreType, "aws")) {
             return S3PropertyReader.getProperty("aws_storage_container");
-        }else {
+        } else {
             throw new ServerException("ERR_INVALID_CLOUD_STORAGE", "Error while getting container name");
         }
     }
@@ -78,5 +78,26 @@ public class CloudStore {
         blob = (Blob) getObject(key);
         String blobString = blob.toString();
         return blobString;
+    }
+
+    public static Map<String, Object> getConfig() {
+        Map<String, Object> cloudConfig = new HashMap<>();
+        String cloudStoreContainer = getContainerName();
+
+        if (StringUtils.equalsIgnoreCase(cloudStoreType, "azure")) {
+            String cloudStoreAccount = Platform.config.getString("azure_storage_key");
+            cloudConfig.put("cloud_store_type", cloudStoreType);
+            cloudConfig.put("cloud_store_account", cloudStoreAccount);
+            cloudConfig.put("cloud_store_container", cloudStoreContainer);
+        } else if (StringUtils.equalsIgnoreCase(cloudStoreType, "aws")) {
+            String cloudStoreAccount = Platform.config.getString("aws_storage_key");
+            cloudConfig.put("cloud_store_type", cloudStoreType);
+            cloudConfig.put("cloud_store_account", cloudStoreAccount);
+            cloudConfig.put("cloud_store_container", cloudStoreContainer);
+        } else {
+            throw new ServerException("ERR_INVALID_CLOUD_STORAGE", "Error while getting container name");
+        }
+
+        return cloudConfig;
     }
 }
